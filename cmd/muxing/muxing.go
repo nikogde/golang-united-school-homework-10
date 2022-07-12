c3b8712d0a14ed6c901aedd91ff92a42540a260d
nikogde <diana.k@ventor.tech@gmail.com>
2022-07-13 02:55:57 +0300

    [ADD] hw-10

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -21,9 +22,50 @@ main function reads host/port from env just for an example, flavor it following 
 func Start(host string, port int) {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/name/{param}", GetName).Methods("GET")
+	router.HandleFunc("/bad", GetBadRequest).Methods("GET")
+	router.HandleFunc("/data", PostData).Methods("POST")
+	router.HandleFunc("/headers", PostHeader).Methods("POST")
+
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func GetName(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	resp := "Hello, " + vars["param"] + "!"
+	w.Write([]byte(resp))
+
+}
+
+func PostData(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+	if err == nil {
+		resp := "I got message:\n" + string(data)
+		w.Write([]byte(resp))
+	}
+}
+
+func GetBadRequest(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func PostHeader(w http.ResponseWriter, r *http.Request) {
+	head := r.Header
+
+	sum := 0
+	for _, key := range []string{"A", "B"} {
+		if val, ok := head[key]; ok {
+			val_int, err := strconv.Atoi(val[0])
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			sum += val_int
+		}
+		w.Header().Set("a+b", strconv.Itoa(sum))
 	}
 }
 
